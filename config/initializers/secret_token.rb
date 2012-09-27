46e6159443c35b99816442cacd0c059c1bb607ac
KathChalmers <accounts@whatsfun.com>
2012-09-26 22:54:50 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mindioso::Application.config.secret_token = '121bf47ec4a12803465cff1f382d8cf707de5c8a315465215f3ad1f22b4d86d1d9e27f7f09a40a35def1382704ff12b7ffdb9ff2fc78953176f2376c49edccf3'
