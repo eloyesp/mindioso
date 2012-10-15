a68cddcc75b625e09b7a3cfde2cd0a7c17877ba5
KathChalmers <accounts@whatsfun.com>
2012-10-14 23:33:15 -0400

    Add Gravitar for contacts

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,2 +1,9 @@
 module ContactsHelper
+
+  def gravatar_for(contact)
+      gravatar_id = Digest::MD5::hexdigest(contact.email.downcase)
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+      image_tag(gravatar_url, alt: (contact.first_name + " " + contact.last_name), class: "gravatar")
+  end
+
 end
